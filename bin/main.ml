0ec540169580fc983daa132fee64061ac1c372bc
cod1r <jasonho353@gmail.com>
2023-10-02 01:02:18 -0500

    Able to read in file and construct leaf nodes with symbol, frequency pairs. Also made warnings non-fatal

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,7 @@
-(*type node =
-    | Leaf of { symbol : char; frequency : int }
-    | Internal of { sum : int; left : node; right : node }
-    | None
-  ;;*)
+type node =
+  | Leaf of { symbol : char; frequency : int }
+  | Internal of { sum : int; left : node; right : node }
+  | None
 
 let usage_msg = "Usage: huffbro <filename>"
 let filename = ref ""
@@ -14,6 +13,34 @@ let set_filename filename_parameter =
 
 let () = Arg.parse [] set_filename ""
 let ic = In_channel.open_bin !filename
-let file_contents_str = In_channel.input_all ic;;
+let file_contents_str = In_channel.input_all ic
 
-print_endline file_contents_str
+let f a c =
+  let is_equal leaf =
+    match leaf with
+    | Leaf { symbol; _ } -> if symbol = c then true else false
+    | _ ->
+        raise
+          (Invalid_argument "Only Leafs should be passed into this function")
+  in
+  if List.exists is_equal a then
+    let item_found = List.find (fun i -> is_equal i) a in
+    let filtered = List.filter (fun i -> is_equal i != true) a in
+    match item_found with
+    | Leaf { symbol = item_found_sym; frequency = item_found_freq } ->
+        Leaf { symbol = item_found_sym; frequency = item_found_freq + 1 }
+        :: filtered
+    | _ ->
+        raise
+          (Invalid_argument "Only Leafs should be passed into this function")
+  else Leaf { symbol = c; frequency = 1 } :: a
+
+let nodes = String.fold_left f [] file_contents_str
+
+let print_leaf leaf =
+  match leaf with
+  | Leaf { symbol; frequency } ->
+      print_endline (Char.escaped symbol ^ " " ^ string_of_int frequency)
+  | _ -> ()
+
+let () = List.iter print_leaf nodes

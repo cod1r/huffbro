1d4d6d585fc2c9b2cfd680d90d0b4af136b904d3
cod1r <jasonho353@gmail.com>
2023-10-01 22:28:19 -0500

    Set up cli argument parsing and handling. Can read in files

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,19 @@
-let () = print_endline "Hello, World!"
+(*type node =
+    | Leaf of { symbol : char; frequency : int }
+    | Internal of { sum : int; left : node; right : node }
+    | None
+  ;;*)
+
+let usage_msg = "Usage: huffbro <filename>"
+let filename = ref ""
+
+let set_filename filename_parameter =
+  match !filename with
+  | "" -> filename := filename_parameter
+  | _ -> raise (Arg.Bad usage_msg)
+
+let () = Arg.parse [] set_filename ""
+let ic = In_channel.open_bin !filename
+let file_contents_str = In_channel.input_all ic;;
+
+print_endline file_contents_str
